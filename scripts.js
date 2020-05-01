b8dd92496a9e0df47665721bbe5a54e9916117ee
Bogdan Anghelache <bogdaan90g@gmail.com>
2020-05-01 22:49:50 +0100

    added some commets

--- scripts.js
+++ scripts.js
@@ -43,11 +43,13 @@ let morse = {
 let array = [];
 let msg = "";
 
+// l-am facut pt a imparti mesajul in litere individuale pt a le putea compara cu obiectul
 for (let i = 0; i < message.length; i++) {
     array.push(message[i]);
 }
-
+// primul loop este pt mesaj iar cel de-al doilea pt obiect (daca le puneam invers, rezultatul era in ordine alfabetica)
 for (let j = 0; j < array.length; j++) {
+    // loop pt obiect
     for (let keys in morse) {
         if (morse[keys] === array[j]) {
             msg += keys;
