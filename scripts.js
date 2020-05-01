61d15c09b5fbe051313089c881603664bc13b4fe
Bogdan Anghelache <bogdaan90g@gmail.com>
2020-05-01 17:08:37 +0100

    morse code convertor

---
+++ scripts.js
@@ -0,0 +1,57 @@
+const message = document.getElementById("input").value;
+const result = document.getElementById("minput");
+
+let morse = {
+    ".-": "A",
+    "-...": "B",
+    "-.-.": "C",
+    "-..": "D",
+    ".": "E",
+    "..-.": "F",
+    "--.": "G",
+    "....": "H",
+    "..": "I",
+    ".---": "J",
+    "-.-": "K",
+    ".-..": "L",
+    "--": "M",
+    "-.": "N",
+    "---": "O",
+    ".--.": "P",
+    "--.-": "Q",
+    ".-.": "R",
+    "...": "S",
+    "-": "T",
+    "..-": "U",
+    "...-": "V",
+    ".--": "W",
+    "-..-": "X",
+    "-.--": "Y",
+    "--..": "Z",
+    ".----": "1",
+    "..---": "2",
+    "...--": "3",
+    "....-": "4",
+    ".....": "5",
+    "-....": "6",
+    "--...": "7",
+    "---..": "8",
+    "----.": "9",
+    "-----": "0",
+};
+
+let array = [];
+let msg = "";
+
+for (let i = 0; i < message.length; i++) {
+    array.push(message[i]);
+}
+
+for (let keys in morse) {
+    for (let j = 0; j < array.length; j++) {
+        if (morse[keys] === array[j]) {
+            msg += keys;
+        }
+    }
+}
+console.log(msg);
